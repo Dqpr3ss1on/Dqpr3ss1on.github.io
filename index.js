7e4ffd2835a971d369471da79fa4a9af915cc326
Dqpr3ss1on <79878809+Dqpr3ss1on@users.noreply.github.com>
2021-03-15 18:31:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -91,19 +91,18 @@ function updateCoin() {
 window.addEventListener("keydown", function(event) {   switch(event.code) {
        case "KeyS":
        case "ArrowDown":
-           //Movement logic down
            break;
+    
        case "KeyW":
        case "ArrowUp":
-           //Movement logic up
            break;
+    
        case "KeyA":
        case "ArrowLeft":
-           //Movement logic left
            break;
+    
        case "KeyD":
        case "ArrowRight":
-           //Movement logic right
            break;
    }
 }, true);
@@ -114,13 +113,13 @@ window.addEventListener("keyup", function(event) {
        case "ArrowDown":
        case "KeyW":
        case "ArrowUp":
-           //Movement logic vertical stop
            break;
+       
        case "KeyA":
        case "ArrowLeft":
        case "KeyD":
        case "ArrowRight":
-           //Movement logic horizontal stop
            break;
+       
    }
 }, true);
