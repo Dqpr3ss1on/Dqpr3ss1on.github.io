e3df40c9698ad98eb56f4605154843f0ff8e88bc
Dqpr3ss1on <79878809+Dqpr3ss1on@users.noreply.github.com>
2021-03-15 17:56:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,76 @@ canvas : document.createElement("canvas"),
   }
 }
 
+let x = 50.0;
+let y = 50.0;
+
+let score = 0;
+
+let coinX = 0.0;
+let coinY = 0.0;
+
+let xVel = 0.0;
+let yVel = 0.0;
+
+const playerColor = "rgb(118,150,86)";
+const screenColor = "rgb(0,0,0)";
+const coinColor = "rgb(221,175,12)";
+const textColor = "rgba(255,255,255, 0.5)";
+
+function game() {
+    canvas.coinInterval++;
+
+    x += xVel;
+    y += yVel;
+
+    if ((x + 100 > coinX && x < coinX + 20) && (y + 100 > coinY && y < coinY + 20)) {
+        score++;
+        canvas.coinInterval = 0;
+        updateCoin();
+    }
+
+    if (canvas.coinInterval === 120) {
+        canvas.coinInterval = 0;
+        updateCoin();
+    }
+
+    if (x < 0.0) {
+        x = 0.0;
+    } else if (x > 700.0) {
+        x = 700.0;
+    }
+
+    if (y < 0.0) {
+        y = 0.0;
+    } else if (y > 700.0) {
+        y = 700.0;
+    }
+
+    let ctx = canvas.context;
+
+    ctx.fillStyle = screenColor;
+    ctx.fillRect(0, 0, 800, 800);
+
+    ctx.fillStyle = playerColor;
+    ctx.fillRect(x, y, 100, 100);
+
+    ctx.fillStyle = coinColor;
+    ctx.fillRect(coinX, coinY, 20, 20);
+
+    ctx.font = "32px Arial";
+    ctx.fillStyle = textColor;
+    ctx.fillText("Score: " + score, 10, 30);
+}
+
+function updateCoin() {
+    coinX = Math.floor((Math.random() * 780));
+    coinY = Math.floor((Math.random() * 780));
+
+    if ((x + 100 > coinX && x < coinX + 20) && (y + 100 > coinY && y < coinY + 20)) {
+        updateCoin();
+    }
+}
+
 window.addEventListener("keydown", function(event) {   switch(event.code) {
        case "KeyS":
        case "ArrowDown":
